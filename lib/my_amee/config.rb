022ef63ab75d0a3e7fb0522e09864b6ebe4cfea1
James Smith <james@floppy.org.uk>
2011-12-14 13:14:00 +0000

    Add global namespace to rails, just in case.

--- lib/my_amee/config.rb
+++ lib/my_amee/config.rb
@@ -6,10 +6,10 @@ module MyAmee
 
     def self.get
       # Load appstore config from YAML
-      my_amee_config = "#{Rails.root}/config/my_amee.yml"
+      my_amee_config = "#{::Rails.root}/config/my_amee.yml"
       if File.exist?(my_amee_config)
         # Load config
-        return YAML.load_file(my_amee_config)[Rails.env]
+        return YAML.load_file(my_amee_config)[::Rails.env]
       else
         Rails.logger.info "WARNING: My AMEE configuration file not found. Copy my_amee.example.yml to config/my_amee.yml and edit."
         return nil
